044e2c66e7f0347054312d97cb5e3ac665eb7e66
hjiayz <hjiayz@163.com>
2020-05-25 09:32:47 +0800

    fix macro

--- examples/define/src/lib.rs
+++ examples/define/src/lib.rs
@@ -5,14 +5,14 @@ pub struct Foo(pub u32);
 
 impl Verify for Foo {
     fn verify(&self) -> bool {
-        (self.0 | 1) == 1
+        (self.0 & 1) == 1
     }
 }
 
 call!( Foo[1] -> u32);
 
 #[derive(Deserialize, Serialize)]
-pub struct Bar(pub u32);
+pub struct Bar;
 
 impl Verify for Bar {}
 subs!( Bar[2] -> (String, u32) );
